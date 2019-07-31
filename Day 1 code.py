9c0a1ed562afdef578c237345ab0ced317c0be3a
abbyseaton101 <53440492+abbyseaton101@users.noreply.github.com>
2019-07-31 10:11:33 -0400

    Add files via upload

---
+++ Day 1 code.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+Hello my name is Abigail
+
+print("My name is Abigail")
+
+print("5*2")
+(5/2)
+5-2
+5**2
+8/9*3
+
+def multiply(a,b):
+    multiply = a*b
+    
+    print(multiply)
+    
+multiply(4,5)
+
+def sum(a,b,c):
+    sum = a+b+c
+    
+    print(sum)
+    
+sum(5,6,9)
+
+def area(a,b,c):
+    area = a*b*c
+    
+    print("The area of the triangle with base, 12 and height 36 is", area)
+    
+area(0.5,12,36)
+
+
+
+
+    
+    
